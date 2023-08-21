33007bfe090f866afd1fbb27bfce9fb80494cc07
Jonathan Yau <jonathan_yau@outlook.com>
2023-08-21 19:59:53 -0400

    added routes to ideas

---
+++ server/ideas.js
@@ -0,0 +1,51 @@
+const ideasRouter = require('express').Router();
+
+module.exports = ideasRouter;
+
+const {
+    getAllFromDatabase,
+    getFromDatabaseById,
+    addToDatabase,
+    updateInstanceInDatabase,
+    deleteFromDatabasebyId
+} = require('./db');
+
+
+
+ideasRouter.param('id', (req, res, next, id) => {
+    const idea = getFromDatabaseById('ideas', id);
+    if (idea) {
+        req.idea = idea;
+        next();
+    } else {
+        res.status(404).send();
+    }
+});
+
+ideasRouter.get('/', (req, res, next) => {
+    res.send(getAllFromDatabase('ideas'));
+});
+
+ideasRouter.post('/', (req, res, next) => {
+    const newIdea = addToDatabase('ideas', req.body);
+    res.status(201).send(newIdea);
+});
+
+ideasRouter.get('/:ideaId', (req, res, next) => {
+    res.send(req.idea);
+});
+
+ideasRouter.put('/:ideaId', (req, res, next) => {
+    const updatedIdeaInstance = updateInstanceInDatabase('ideas', req.body);
+    res.send(updatedIdeaInstance);
+});
+
+ideasRouter.delete('/:ideaId', (req, res, next) => {
+    const deleted = deleteFromDatabasebyId('ideas', req.params.id);
+    if (deleted) {
+        res.status(204);
+    } else {
+        res.status(500);
+    }
+    res.send();
+});
--- server/minions.js
+++ server/minions.js
@@ -4,7 +4,7 @@ module.exports = minionsRouter;
 
 const {
     getAllFromDatabase, 
-    getFromDatabaseById
+    getFromDatabaseById,
     addToDatabase, 
     updateInstanceInDatabase, 
     deleteFromDatabasebyId
