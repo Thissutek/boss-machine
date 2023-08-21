fc50e550bbdacf464960daa4b436678e0edc5f81
Jonathan Yau <jonathan_yau@outlook.com>
2023-08-20 21:04:30 -0400

    finished the minions routes

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
-const cors = require('cors');
 
 module.exports = app;
 app.use(express.static(__dirname));
@@ -11,14 +9,16 @@ app.use(express.static(__dirname));
 const PORT = process.env.PORT || 4001;
 
 // Add middleware for handling CORS requests from index.html
-
+const cors = require('cors');
+app.use(cors());
 
 // Add middware for parsing request bodies here:
-
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
 
 // Mount your existing apiRouter below at the '/api' path.
 const apiRouter = require('./server/api');
-
+app.use('/api', apiRouter);
 
 // This conditional is here for testing purposes:
 if (!module.parent) { 
